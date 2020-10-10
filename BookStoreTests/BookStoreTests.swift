8ec98f25a510caec26ab67877ae52af96a563770
Kaio Dantas <ikaioi@hotmail.com>
2020-10-10 12:08:10 -0300

    Adicionados alguns UnitTests

--- BookStoreTests/BookStoreTests.swift
+++ BookStoreTests/BookStoreTests.swift
@@ -10,6 +10,78 @@ import XCTest
 
 class BookStoreTests: XCTestCase {
 
+    
+//    var systemUnderTest: ViewController
+    
+    lazy var systemUnderTest: ViewController = {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    }()
+    
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSUT_CanInstantiateViewController() {
+        
+        XCTAssertNotNil(systemUnderTest)
+    }
+    
+    func testSUT_HasItemsForCollectionView() {
+        
+        XCTAssertNotNil(systemUnderTest.bookList)
+    }
+    
+    
+    func testSUT_ConformsToCollectionViewDataSource() {
+        
+        XCTAssert(systemUnderTest.conforms(to: UICollectionViewDataSource.self))
+    }
+    
+    
+    func testSUT_ConformsToCollectionViewDelegateFlowLayout () {
+        
+        XCTAssert(systemUnderTest.conforms(to: UICollectionViewDelegateFlowLayout.self))
+  
+    }
+    
+    
+    
+    var error: Error? {
+        didSet {
+            print("Error on request")
+        }
+    }
+    
+    
+    
+    func testValidSearch() {
+        
+        let searchText = "ios"
+        let page = 0
+        let perpage = 50
+        
+        
+        
+        let dataService = DataService()
+        dataService.requestBooksList(with: searchText, maxResults: perpage, startIndex: page, completion: { (booksResult, error) in
+            if let error = error {
+                self.error = error;
+                XCTFail("Error on request")
+                return
+            }
+            
+            XCTAssert(true, "Success")
+            
+        })
+        
+    }
+    
+    
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
